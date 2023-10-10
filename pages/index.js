ca75d465c9e04c826426929e8e3385db8369e463
jespermoseti <72150431+jespermoseti@users.noreply.github.com>
2023-10-10 14:25:28 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,38 @@
 import { MongoClient } from "mongodb";
-import Image from "next/image";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
 import classes from "./home.module.css";
 
 function Home(props) {
   const descriptiondata = JSON.parse(props.data);
+
   return (
     <section className={classes.homepage}>
-      <div className={classes.carousel}>
+      <div className={classes.carouselContainer}>
         <Carousel
           interval={5000}
-          autoPlay="autoPlay"
+          autoPlay={true}
           infiniteLoop={true}
           showThumbs={false}
+          showArrows={true}
         >
           <div>
-            <img src="/images/carousel1.jpg" alt="image1" layout="fill" />
+            <img src="/images/carousel1.jpg" alt="image1" />
             <p className="legend">Techonsolutions</p>
           </div>
           <div>
-            <img src="/images/carousel2.jpg" alt="image2" layout="fill" />
+            <img src="/images/carousel2.jpg" alt="image2" />
             <p className="legend">We offer IT services in Kenya</p>
           </div>
           <div>
-            <img src="/images/carousel3.jpg" alt="image3" layout="fill" />
+            <img src="/images/carousel3.jpg" alt="image3" />
             <p className="legend">
               Networking | Consultancy | Cybersecurity | Web development | Cloud
               computing | IT Support
             </p>
           </div>
           <div>
-            <img src="/images/carousel4.jpg" alt="image4" layout="fill" />
+            <img src="/images/carousel4.jpg" alt="image4" />
             <p className="legend">
               techonsolutions@yahoo.com | +254705933595 | Social media:
               @techonsolns
@@ -39,53 +40,21 @@ function Home(props) {
           </div>
         </Carousel>
       </div>
-
-      {/* <video
-          autoPlay
-          loop
-          muted
-          className={classes.video}
-          style={{
-            width: "100%",
-            height: "100%",
-            zIndex: "-1",
-            borderRadius: "0.5rem",
-          }}
-        >
-          <source src="/images/city.mp4" type="video/mp4" />
-        </video> */}
-      {descriptiondata.map((item) => (
-        <p key={item._id}>{item.description}</p>
-      ))}
-      
-      {/* <Image
-        className={classes.backgroundImage}
-        src="/images/wallpaper.jpg"
-        alt="an image showing jes"
-        //layout="fill"
-        width={600}
-        height={400}
-        objectFit="cover"
-        objectPosition="center"
-      /> */}
+      <div className={classes.descriptionContainer}>
+        {descriptiondata.map((item) => (
+          <p key={item._id}>{item.description}</p>
+        ))}
+      </div>
     </section>
   );
 }
 
 export async function getStaticProps(context) {
-  // const client = await MongoClient.connect(
-  //   "mongodb+srv://<username>:<password>@<clustername>.ibpnt47.mongodb.net/<database>?retryWrites=true&w=majority"
-  // );
-
   const connectionString = `mongodb+srv://${process.env.mongodb_username}:${process.env.mongodb_password}@${process.env.mongodb_clustername}.ibpnt47.mongodb.net/${process.env.mongodb_database}?retryWrites=true&w=majority`;
   const client = await MongoClient.connect(connectionString);
-
   const db = client.db();
-
   const dataarray = await db.collection("descriptiondetails").find().toArray();
-
   const array = JSON.stringify(dataarray);
-
   client.close();
 
   return {
@@ -95,4 +64,5 @@ export async function getStaticProps(context) {
     revalidate: 60,
   };
 }
+
 export default Home;
