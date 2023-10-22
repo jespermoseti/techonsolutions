fb29ab156199459d0f2293622d88686f4343537e
jespermoseti <72150431+jespermoseti@users.noreply.github.com>
2023-10-22 15:12:07 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -61,9 +61,9 @@ function Home(props) {
         </p>
         <ul>
           <li>
-            <strong>A World of Services:</strong> TechonSolutions is your
+            <strong>A World of Services:</strong> <p>TechonSolutions is your
             gateway to a vast array of IT services. From Nairobi to New York,
-            our team is ready to serve you with services including:
+            our team is ready to serve you with services including:</p>
             <ul>
               <li><p>Networking: Streamline your connectivity and ensure
               seamless communication within your organization.</p></li>
@@ -90,15 +90,15 @@ function Home(props) {
             </ul>
           </li>
           <li>
-            <strong>Global Reach, Local Expertise:</strong> Although based in
+            <strong>Global Reach, Local Expertise:</strong> <p>Although based in
             Nairobi, we have a global perspective. Our solutions transcend
             borders, and we work with clients worldwide to transform their IT
-            landscape.
+            landscape.</p>
           </li>
           <li>
-            <strong>Customer-Centric Approach:</strong> Our commitment to
+            <strong>Customer-Centric Approach:</strong> <p>Our commitment to
             understanding your unique needs, goals, and challenges ensures that
-            we deliver tailored, customer-focused solutions.
+            we deliver tailored, customer-focused solutions.</p>
           </li>
         </ul>
         <p>
