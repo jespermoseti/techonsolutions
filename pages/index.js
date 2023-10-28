9d80d783eeddb68ba1dcaae8c603ccfaca85893d
jespermoseti <72150431+jespermoseti@users.noreply.github.com>
2023-10-28 12:06:53 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -44,9 +44,6 @@ function Home(props) {
           <div>
             <img src="/images/carousel5.jpg" alt="image4" />
           </div>
-          <div>
-            <img src="/images/carousel6.jpg" alt="image4" />
-          </div>
         </Carousel>
       </div>
         <hr style={hrStyle} />
