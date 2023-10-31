1fbe29ba0356950b770f4b7b5939939e7c47f720
jespermoseti <72150431+jespermoseti@users.noreply.github.com>
2023-10-31 20:24:28 +0300

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -5,37 +5,38 @@ function About(props) {
   const aboutdata = JSON.parse(props.data);
   return (
     <div className={classes.aboutpage}>
-      <div className={classes.about-overlay}></div>
-      <div className={classes.about-content}>
-        <h1>About TechonSolutions</h1>
+      <h1>About TechonSolutions</h1>
         <p>
           At TechonSolutions, we are your trusted IT partner, dedicated to
           providing a wide range of IT solutions that empower businesses and
           individuals with cutting-edge technology. Our commitment to excellence
-          and a customer-centric approach set us apart.
+          and customer-centric approach set us apart.
         </p>
 
-        <h3>Why Choose Us?</h3>
+        <p>
+          <h3>Why Choose Us?</h3>
+        </p>
         <ul>
           <li>
             <p>
               <strong>Expertise:</strong> Our team of skilled technicians and IT
-              experts bring years of experience to the table, ensuring your IT
-              needs are met with precision and professionalism.
+              experts brings years of experience to the table, ensuring your
+              IT needs are met with precision and professionalism.
             </p>
           </li>
           <li>
             <p>
-              <strong>Comprehensive Services:</strong> From networking and web
-              development to cybersecurity and software solutions, we offer a
-              wide spectrum of services designed to streamline your IT operations.
+              <strong>Comprehensive Services:</strong> From networking and
+              web development to cybersecurity and software solutions, we offer
+              a wide spectrum of services designed to streamline your IT
+              operations.
             </p>
           </li>
           <li>
             <p>
-              <strong>Global Perspective:</strong> While we are based in Nairobi,
-              our vision extends globally. We work with clients worldwide to
-              transform their IT landscape.
+              <strong>Global Perspective:</strong> While we are based in
+              Nairobi, our vision extends globally. We work with clients
+              worldwide to transform their IT landscape.
             </p>
           </li>
           <li>
@@ -48,26 +49,33 @@ function About(props) {
           </li>
           <li>
             <p>
-              <strong>Swift Computer Repairs:</strong> We also offer reliable
-              computer repair services to get you back to work hassle-free. Do not
-              let computer problems hold you back.
+              <strong>Swift Computer Repairs:</strong> We also offer
+              reliable computer repair services to get you back to work
+              hassle-free. Do not let computer problems hold you back.
             </p>
           </li>
         </ul>
-        {aboutdata.map((item) => (
-          <p key={item._id}>{item.about}</p>
-        ))}
-      </div>
+      {aboutdata.map((item) => (
+        <p key={item._id}>{item.about}</p>
+      ))}
     </div>
   );
 }
 
 export async function getStaticProps(context) {
+  // const client = await MongoClient.connect(
+  //   "mongodb+srv://<username>:<password>@<clustername>.ibpnt47.mongodb.net/<database>?retryWrites=true&w=majority"
+  // );
+
   const connectionString = `mongodb+srv://${process.env.mongodb_username}:${process.env.mongodb_password}@${process.env.mongodb_clustername}.ibpnt47.mongodb.net/${process.env.mongodb_database}?retryWrites=true&w=majority`;
   const client = await MongoClient.connect(connectionString);
+
   const db = client.db();
+
   const dataarray = await db.collection("aboutdetails").find().toArray();
+
   const array = JSON.stringify(dataarray);
+
   client.close();
 
   return {
