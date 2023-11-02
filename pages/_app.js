fc87960d55f2fea955801940933106b86be25e40
jespermoseti <72150431+jespermoseti@users.noreply.github.com>
2023-11-02 23:57:12 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -19,8 +19,52 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }) {
             />
             <meta
               name="description"
-              content="Techonsolutions information technology IT solutions and services Nairobi Kenya"
+              content="Techonsolutions information technology IT solutions and services in Nairobi Kenya 
+                Networking installations web-development consultancy software cloud-computing graphics design
+                CCTV cybersecurity computer repairs"
             />
+
+            <!-- Facebook Open Graph tags -->
+                <meta property="og:title" content="Techonsolutions" />
+                <meta property="og:description" content="At TechonSolutions, we are dedicated to delivering cutting-edge IT solutions
+                that empower businesses and individuals alike. Whether you're a startup seeking a digital edge or an established
+                organization looking to optimize your IT infrastructure, we're here to elevate your possibilities and transform your
+                IT landscape." />
+                <meta property="og:image" content="/images/carousel4.jpg" />
+
+            <!-- Twitter Cards -->
+                <meta name="twitter:card" content="summary" />
+                <meta name="twitter:title" content="Techonsolutions" />
+                <meta name="twitter:description" content="At TechonSolutions, we are dedicated to delivering cutting-edge IT solutions
+                that empower businesses and individuals alike. Whether you're a startup seeking a digital edge or an established
+                organization looking to optimize your IT infrastructure, we're here to elevate your possibilities and transform your
+                IT landscape." />
+                <meta name="twitter:image" content="/images/carousel4.jpg" />
+
+            <!-- Schema Markup -->  
+                <script type="application/ld+json" dangerouslySetInnerHTML={{
+                    __html: JSON.stringify({
+                      "@context": "http://schema.org",
+                      "@type": "Organization",
+                      "name": "TechonSolutions",
+                      "url": "https://techonsolutions.com/",
+                      "description": "IT services and solutions company",
+                      "logo": "/images/favicon.ico",
+                      "address": {
+                        "@type": "PostalAddress",
+                        "streetAddress": "Nairobi",
+                        "addressLocality": "City",
+                        "addressRegion": "",
+                        "postalCode": "",
+                        "addressCountry": "Kenya"
+                      },
+                      "contactPoint": {
+                        "@type": "ContactPoint",
+                        "telephone": "+254 113270070",
+                        "contactType": "Customer service and Enquiry"
+                      }
+                    })
+                  }} />
           </Head>
           <Component {...pageProps} />
           <MessengerCustomerChat
