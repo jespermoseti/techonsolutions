49cb0e34bf22b8d110e894c89783f9fad49885c4
jespermoseti <72150431+jespermoseti@users.noreply.github.com>
2023-11-02 21:21:27 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -85,7 +85,7 @@ function Home(props) {
               data-swiper-parallax={"23%"}
               className="slider-bg"
               style={{
-                backgroundImage: `url('https://source.unsplash.com/random/1000x500/?Africans,office.workplace')`
+                backgroundImage: `url('https://source.unsplash.com/random/1000x500/?modern,office,workplace')`
               }}
             />
           </div>
