3d86c3c58c0dd3125f68e471d12aab1f57811b27
jespermoseti <72150431+jespermoseti@users.noreply.github.com>
2023-11-08 14:07:53 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -72,14 +72,14 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }) {
 
           <Script
           strategy="afterInteractive"
-          src={`https://www.googletagmanager.com/gtag/js?id=${measurementId}`}
+          src={`https://www.googletagmanager.com/gtag/js?id=G-CF64JTKL46`}
           />
           <Script id="google-analytics" strategy="afterInteractive">
             {`
                 window.dataLayer = window.dataLayer || [];
                 function gtag(){dataLayer.push(arguments);}
                 gtag('js', new Date());
-                gtag('config', '${measurementId}');
+                gtag('config', 'G-CF64JTKL46');
             `}
           </Script>
 
